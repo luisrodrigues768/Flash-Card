62575874d39732551d6e78f2c7f45f8f5e8f7c52
L.O Rodrigues <132573705+luisrodrigues768@users.noreply.github.com>
2024-10-09 09:54:46 -0300

    iniciando o js

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao (
+    'Matemática',
+    'Qual é o Teorema de Pitágoras?',
+    'O Teorema de Pitágoras é a²=b²+c².'
+)
+
+criaCartao (
+    'Biologia',
+    'Qual a Primeira Lei de Mendel?',
+    'A Primeira Lei de Mendel é a lei da segregação dos fatores.'
+)
+
+criaCartao (
+    'História',
+    'Qual foi o primeiro presidente do Brasil?',
+    'O primeiro presidente do Brasil foi o Marechal Deodoro da Fonseca.'
+)
+
