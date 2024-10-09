e45f232e0378f767feb5c5f94aec056dd5133026
L.O Rodrigues <132573705+luisrodrigues768@users.noreply.github.com>
2024-10-09 10:28:03 -0300

    configurando o js

--- perguntas.js
+++ perguntas.js
@@ -16,3 +16,38 @@ criaCartao (
     'O primeiro presidente do Brasil foi o Marechal Deodoro da Fonseca.'
 )
 
+criaCartao (
+    'Geografia',
+    'Qual o maior rio do mundo?',
+    'O maior rio do mundo é o rio Amazonas.'
+)
+
+criaCartao (
+    'Química',
+    'Quais a fórmula química da água?',
+    'A fórmula química da água é H2O.'
+)
+
+criaCartao (
+    'Português',
+    'Quais são os quatro porquês?',
+    'Os quatro porquês são: por que, porque, por quê e porquê.'
+)
+
+criaCartao (
+    'Inglês',
+    'O que é o verbo to be?',
+    'O verbo to be é o verbo ser e estar, composto pelos verbos "Am", "Are" e "Is".'
+)
+
+criaCartao (
+    'Física',
+    'Quem foi o físico que criou a teoria da relatividade geral?',
+    'Foi o físico alemão Albert Einstein.'
+)
+
+criaCartao (
+    'Educação física',
+    'Qual o nome do evento que reúne várias modalidades e acontece a cada 4 anos',
+    'Esse evento é os Jogos Olímpicos.'
+)
